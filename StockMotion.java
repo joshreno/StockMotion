a198a43f6ec51368882d569e550b662a8f764232
jreno3 <joshuagregr@gmail.com>
2017-07-17 10:02:42 -0400

    first commit

--- StockMotion.java
+++ StockMotion.java
@@ -1,12 +1,20 @@
 // import leap motion
 
-public class StockMotion {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+
+public class StockMotion extends Application{
     private Date stockStartDate;
     private Date stockEndDate;
 
-
-    public StockMotion() {
+    public void start(Stage primaryStage) {
         stockStartDate = new Date();
         stockEndDate = new Date();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
