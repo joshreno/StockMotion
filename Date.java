90f40679e57b2f74c187f116f8475c667ab9a2eb
jreno3 <joshuagregr@gmail.com>
2017-07-16 22:23:22 -0400

    first commit

---
+++ Date.java
@@ -0,0 +1,86 @@
+// import the current day
+public class Date {
+    private Month month;
+    private int day;
+    private int year;
+
+    /**
+     * Sets the day, year, and month
+     * @param day
+     * @param year
+     * @param month
+    */
+    public Date(int day, int year, Month month) {
+        this.day = day;
+        this.year = year;
+        this.month = month;
+    }
+
+    /**
+     * Sets the day
+     * @param day
+    */
+    public Date() {
+        this(0, 0, Month.JANUARY);
+    }
+
+    /**
+     * Sets the day
+     * @param day
+    */
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    /**
+     * Returns the day
+     * @return day
+    */
+    public int getDay() {
+        return day;
+    }
+
+    /**
+     * Sets the year
+     * @param year
+    */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    /**
+     * Returns the year
+     * @return year
+    */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * Sets the month
+     * @param month
+    */
+    public void setMonth(Month month) {
+        this.month = month;
+    }
+
+    /**
+     * Returns the month
+     * @return month
+    */
+    public Month getMonth() {
+        return month;
+    }
+
+    /**
+     * Sets the day, year, and month
+     * @param day
+     * @param year
+     * @param month
+    */
+    public void setDate(int day, int year, Month month) {
+        this.day = day;
+        this.year = year;
+        this.month = month;
+    }
+}
---
+++ StockMotion.java
@@ -0,0 +1,12 @@
+// import leap motion
+
+public class StockMotion {
+    private Date stockStartDate;
+    private Date stockEndDate;
+
+
+    public StockMotion() {
+        stockStartDate = new Date();
+        stockEndDate = new Date();
+    }
+}
