982110522f4a3ed89d0498510b9751edd4130571
jreno3 <joshuagregr@gmail.com>
2017-07-28 22:59:26 -0400

    updated areachart

--- src/StockChart.java
+++ src/StockChart.java
@@ -4,17 +4,20 @@ import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
+import javafx.collections.ObservableList;
 import javafx.scene.chart.*;
 
 /**
  * Created by joshuareno on 7/26/17.
  */
+
 public class StockChart {
-    private static AreaChart<Date, Double> areaChart;
+    private static AreaChart<String, Double> areaChart;
     private static NumberAxis yAxis;
     private static CategoryAxis xAxis;
     private static List<Date> listOfDates = new ArrayList<Date>();
     private static List<Double> listOfAdjClose = new ArrayList<Double>();
+    private static List<String> listOfStringDates = new ArrayList<String>();
     private static List<yahoofinance.histquotes.HistoricalQuote> listOfQuotes;
     private static XYChart.Series series;
 
@@ -30,9 +33,12 @@ public class StockChart {
             Double close = histQuote.getAdjClose().doubleValue();
             listOfAdjClose.add(close);
             Date date = Date.Date(histQuote.getDate());
-            listOfDates.add(date);
-            series.getData().add(new XYChart.Data(date, close));
+            listOfStringDates.add(date.toString());
+            series.getData().add(new XYChart.Data(date.toString(), close));
         }
+        xAxis = new CategoryAxis((ObservableList) listOfStringDates);
+        yAxis = new NumberAxis(0, stock.getQuote().getPrice().doubleValue(), stock.getQuote().getPrice().doubleValue()/100);
+        areaChart = new AreaChart<String, Double>(xAxis, (Axis) yAxis);
         areaChart.getData().addAll(series);
         return StockChart.areaChart;
     }
